a4f2c59bc3c149327a1aa59f831b5de01aa393cf
mechztcc <dojathan@gmail.com>
2023-06-17 12:12:20 -0300

    feat: added index users route

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,13 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user-dto/create-user-dto';
 import { CreateUserService } from '../services/create-user/create-user.service';
+import { FindAllUsersServiceService } from '../services/find-all-users-service/find-all-users-service.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private createUserService: CreateUserService) {}
+  constructor(
+    private createUserService: CreateUserService,
+    private findAllUsersService: FindAllUsersServiceService,
+  ) {}
   @Post()
   async create(@Body() createUserDTO: CreateUserDto) {
     const users = await this.createUserService.execute(createUserDTO);
+    return users;
+  }
+
+  @Get()
+  async index() {
+    const users = await this.findAllUsersService.execute();
     return users;
   }
 }
